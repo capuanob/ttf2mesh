2b2b913d7fb5c4f0619ef3615defe1c0fffd7adb
bcapuano <bcapuano@asu.edu>
2023-04-23 12:29:55 -0400

    Added more coverage to harness

--- mayhem/fuzz_lib.cpp
+++ mayhem/fuzz_lib.cpp
@@ -37,14 +37,35 @@ class Font {
     void load_svg() const {
         ttf_glyph2svgpath(&font->glyphs[0], 0.1f, 0.2f);
     }
+
+    void splitted_outline() const {
+        auto *o = ttf_splitted_outline(&font->glyphs[0]);
+        float p, q;
+        if (o != nullptr) {
+            ttf_outline_evenodd_base(o, &p, 0, &q);
+        }
+        ttf_free_outline(o);
+    }
+
+    void linear_outline() const {
+        auto *o = ttf_linear_outline(&font->glyphs[0], TTF_QUALITY_NORMAL);
+        float p;
+        if (o != nullptr) {
+            ttf_outline_evenodd(o, &p, 0);
+        }
+        ttf_free_outline(o);
+    }
 };
 
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    if (size < 500) return -1;
 
     FuzzedDataProvider fdp(data, size);
 
-    const auto symbol = static_cast<wchar_t>('A');
+    const auto symbol = static_cast<wchar_t>(data[407]);
+    const auto choice = data[408] % 5;
+
     const std::size_t font_size = fdp.remaining_bytes();
     std::unique_ptr<uint8_t[]> data_ptr(new uint8_t[font_size]);
     fdp.ConsumeData(data_ptr.get(), font_size);
@@ -56,13 +77,29 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         return -1;
     }
 
-//     Find the symbol in the font
+    // Find the glyph
     int index = ttf_find_glyph(font.font, symbol);
     if (index < 0) return -1;
-    // Make mesh object from the glyph
 
-    font.load_mesh();
-    font.load_3D_mesh();
-    font.load_svg();
+    switch(choice) {
+        case 0:
+            font.load_mesh();
+            break;
+        case 1:
+            font.load_3D_mesh();
+            break;
+        case 2:
+            font.load_svg();
+            break;
+        case 3:
+            font.splitted_outline();
+            break;
+        case 4:
+            font.linear_outline();
+            break;
+        default:
+            return -1;
+    }
+
     return 0;
 }
